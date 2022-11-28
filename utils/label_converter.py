47dabce35fe64277fa873e3a86fc4486cf641701
Franz Sauerwald <15076254+FranzSw@users.noreply.github.com>
2022-11-28 15:41:13 +0100

    Add label converter for brno dataset
    Former-commit-id: adb207bfbfdd741ce19851043df05410b576dc10
    Former-commit-id: 87fd42e0d494d48aeb7485031ebf37220b795b34

---
+++ utils/label_converter.py
@@ -0,0 +1,55 @@
+import pickle
+import pickle_compat
+import pandas as pd
+from pathlib import Path
+pickle_compat.patch()
+
+brno_dataset_path = '/Volumes/X/datasets/2016-ITS-BrnoCompSpeed/dataset/'
+
+# How to use converted labels
+# -- Only pandas import necesary
+
+# cars = pd.read_csv('cars.csv')
+
+# def avg_speed_for_time(timeStart, timeEnd):
+#     cars_to_avg = cars.loc[cars['start'].gt(timeStart) & cars['end'].le(timeEnd)]
+#     return cars_to_avg['speed'].mean()
+
+# avg_speed_for_time(50, 300)
+
+
+def list_pkl_files_in_folder(target: Path):
+    pkl_list = []
+    for file in target.iterdir():
+        if file.is_dir():
+            pkl_list += list_pkl_files_in_folder(file)
+        elif file.name == 'gt_data.pkl':
+            pkl_list.append(file)
+    return pkl_list
+
+def map_car_to_entry(car):
+    if car['valid'] is not True:
+        return None
+    intersection_times = [x['videoTime'] for x in car['intersections']]
+    intersection_times = sorted(intersection_times)
+    if len(intersection_times) < 2:
+        print(car)
+    return (intersection_times[0], intersection_times[1], car['carId'], car['speed'])
+
+def main():
+    pkl_to_convert = list_pkl_files_in_folder(Path(brno_dataset_path))
+    for pkl in pkl_to_convert:
+        out_path = Path(pkl).with_name('cars.csv')
+
+        print(f'Converting {pkl} to {out_path}')
+
+        with open(pkl, "rb") as f:
+            pkl_obj = pickle.load(f)
+
+        mapped_list = list(map(map_car_to_entry, pkl_obj['cars']))
+        mapped_list = filter(None, mapped_list)
+        df = pd.DataFrame.from_records(mapped_list, columns=['start', 'end', 'carId', 'speed'])
+        df.to_csv(out_path, index=False)
+    
+if __name__ == '__main__':
+    main()
