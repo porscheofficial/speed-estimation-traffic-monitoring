731d968388b1af6d6e1dd60b19b1e764304bce85
Sylwester Sawicki <sylwester.sawicki@student.hpi.de>
2022-12-14 19:20:28 +0100

    [ADD] resizer
    Former-commit-id: 7115e178d8ce3aeee3c0492b58dfc756dd26466f
    Former-commit-id: ee77c5b7376690dae77f68dc440f34a120f79b93

--- object-detection-yolo/paths.py
+++ object-detection-yolo/paths.py
@@ -1,7 +1,7 @@
-path_to_haar_file = "dnn_model/myhaar.xml"
-yolo_weights = "dnn_model/yolov4.weights"
-yolo_classes = "dnn_model/classes.txt"
-yolo_config = "dnn_model/yolov4.cfg"
+path_to_haar_file = "object-detection-yolo/dnn_model/myhaar.xml"
+yolo_weights = "object-detection-yolo/dnn_model/yolov4.weights"
+yolo_classes = "object-detection-yolo/dnn_model/classes.txt"
+yolo_config = "object-detection-yolo/dnn_model/yolov4.cfg"
 
 cars_path = "/Users/saver/Documents/master/semester-1/AIP/brno_complete/2016-ITS-BrnoCompSpeed/dataset/session0_right/"
-path_to_dataset = "../datasets/yt_video1.mp4"
+path_to_dataset = cars_path + "video.mp4"
