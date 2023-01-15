bf97e4d6cf3cce3765056066c20c8ac182638fa8
Ahmad Ahmadov <109951824+ahmadahmadov@users.noreply.github.com>
2023-01-15 19:34:34 +0000

    correct sorting of the frames
    Former-commit-id: 2bea8c43c80556e080bdced16b3e9245be0a4878
    Former-commit-id: b1232348c359b5994ce7a37bb7de13c408845c77

--- frames_to_json.py
+++ frames_to_json.py
@@ -1,55 +1,12 @@
 import json
 import os
 import glob
-import pathlib
 
-inputPath ='/Users/your_username/Documents/os/frames_HamptonRoads/*.png'
-
-testImages = glob.glob(inputPath)
-
-
-num = 0
-
-for image in testImages:
-    num = num + 1
-
-print(num)
-
-i = 0
-
-listObj = []
-filename = 'test_pytorch_HamptonRoads.json'
-#dataName = 'HamptonRoads864'
-
-#with open("train_pytorch_4KVideoOfHighwayTraffic.json", "w") as fp:
-
-
-for image in sorted(testImages):
-    #path = os.path.relpath(image)
-    path = pathlib.Path(image).parent.resolve()
-    dataName = os.path.splitext(image)[0]
-
-    if i > num - 2:
-        break
-
-    else:
-        #name1 = str(path)+'/'+ dataName +str(i)+'.png'
-        #name2 = str(path)+'/'+ dataName +str(i+1)+'.png'
-
-        name1 = dataName +str(i)+'.png'
-        name2 = dataName +str(i+1)+'.png'
-
-        new_elements = {i : [name1, name2]}
-
-        with open(filename) as fp:
-            listObj = json.load(fp)
-
-        #print(listObj)
-        
-        listObj.append(new_elements)
-
-
-        with open(filename, "w") as outfile:
-            json.dump(listObj, outfile)    
-        
-        i=i+1
+#number extraction is hard coded. Better idea would be to delimitate order index from the filename by a "_"
+sorted_frames = sorted([os.path.basename(x) for x in glob.glob('./images/*.png')], key=lambda x: int(x[14:-4]))
+json_list = {}
+for i, filename in enumerate(sorted_frames[:-1]):
+    json_list[str(i)]=[os.path.join('./images/',sorted_frames[i]), os.path.join('./images/',sorted_frames[i+1])]
+    
+with(open("./data/train.json",'w')) as fout:
+    json.dump(json_list, fout)
