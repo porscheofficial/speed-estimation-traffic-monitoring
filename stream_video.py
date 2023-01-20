94f4b5310d1dad8b27345e94372c02d0e6ddd84e
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-01-20 20:28:58 +0100

    add fps estimation for streams
    Former-commit-id: ff74c60ce3c26313e6b849841df45ef17d283b3a
    Former-commit-id: 77ac7f0cad055821a98eb918acf1af4c7f08c2f0

--- stream_video.py
+++ stream_video.py
@@ -2,6 +2,7 @@ import cv2
 import subprocess as sp
 import numpy
 import argparse
+import time
 
 
 def get_video_stream_from_url(url):
@@ -16,7 +17,7 @@ def get_video_stream_from_url(url):
                          stdin=sp.PIPE, stdout=sp.PIPE)
     frame_size = meta_pipe.stdout.read().decode("utf-8").split("\n")[0].split(",")
 
-    stream_pipe = sp.Popen([FFMPEG_BIN, "-i", VIDEO_URL,
+    stream_pipe = sp.Popen([FFMPEG_BIN, "-i", url,
                             "-loglevel", "quiet",  # no text output
                             "-an",  # disable audio
                             "-f", "image2pipe",
@@ -31,6 +32,16 @@ def retrieve_next_frame_from_stream(pipe, w, h):
     return numpy.frombuffer(raw_image, numpy.uint8).reshape((h, w, 3))
 
 
+def count_fps_from_stream(pipe, w, h):
+    n_frames = 1000
+    start_time = time.time()
+    for _ in range(n_frames):
+        retrieve_next_frame_from_stream(pipe, w, h)
+    end_time = time.time()
+    delta_t = end_time - start_time
+    return n_frames / delta_t
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Show an m3u8 stream over http')
     parser.add_argument(
@@ -39,11 +50,12 @@ if __name__ == '__main__':
         help='Enter the link to an m3u8 stream over http',
     )
 
-    stream, width, height = get_video_stream_from_url(parser.parser.parse_args().stream_link)
+    stream, width, height = get_video_stream_from_url(parser.parse_args().stream_link)
+    fps = count_fps_from_stream(stream, width, height)
 
     while True:
         cv2.imshow("Stream", retrieve_next_frame_from_stream(stream, width, height))
-        if cv2.waitKey(30) == ord('q'):
+        if cv2.waitKey(int(1000 / fps)) == ord('q'):
             break
 
     cv2.destroyAllWindows()
