cef23f5a4857287c9f7ba2edf802dfe9c2d829b6
Lucas Liebe <lucasliebe.ll@gmail.com>
2023-01-20 18:00:08 +0100

    cleanup stream file
    Former-commit-id: c75f5f9dfcbc7e1340a03d79efe4658ea390c076
    Former-commit-id: a84536777792da70176345e869dab8341081b87c

--- stream_video.py
+++ stream_video.py
@@ -1,53 +1,49 @@
 import cv2
 import subprocess as sp
 import numpy
+import argparse
 
-FFMPEG_BIN = "ffmpeg"
-FFPROBE_BIN = "ffprobe"
-VIDEO_URL = "https://49-d2.divas.cloud/CHAN-8293/CHAN-8293_1.stream/playlist.m3u8?217.232.115.59&vdswztokenhash=M27XmYvnp9yZ4EQO-Gq1GQbSCYhwCMj6LlGrX_vBiuc="
 
-meta_pipe = sp.Popen([FFPROBE_BIN, "-v", "error",
-                      "-select_streams", "v:0",
-                      "-show_entries", "stream=width,height",  # disable audio
-                      "-of", "csv=p=0",
-                      VIDEO_URL],
-                     stdin=sp.PIPE, stdout=sp.PIPE)
-frame_size = meta_pipe.stdout.read().decode("utf-8").split("\n")[0].split(",")
-width = int(frame_size[0])
-height = int(frame_size[1])
+def get_video_stream_from_url(url):
+    FFMPEG_BIN = "ffmpeg"
+    FFPROBE_BIN = "ffprobe"
 
-stream_pipe = sp.Popen([FFMPEG_BIN, "-i", VIDEO_URL,
-                        "-loglevel", "quiet",  # no text output
-                        "-an",  # disable audio
-                        "-f", "image2pipe",
-                        "-pix_fmt", "bgr24",
-                        "-vcodec", "rawvideo", "-"],
-                       stdin=sp.PIPE, stdout=sp.PIPE)
-while True:
-    raw_image = stream_pipe.stdout.read(width * height * 3)
-    image = numpy.frombuffer(raw_image, numpy.uint8).reshape((height, width, 3))
-    cv2.imshow("Stream", image)
-    if cv2.waitKey(30) == ord('q'):
-        break
+    meta_pipe = sp.Popen([FFPROBE_BIN, "-v", "error",
+                          "-select_streams", "v:0",
+                          "-show_entries", "stream=width,height",  # disable audio
+                          "-of", "csv=p=0",
+                          url],
+                         stdin=sp.PIPE, stdout=sp.PIPE)
+    frame_size = meta_pipe.stdout.read().decode("utf-8").split("\n")[0].split(",")
 
-cv2.destroyAllWindows()
+    stream_pipe = sp.Popen([FFMPEG_BIN, "-i", VIDEO_URL,
+                            "-loglevel", "quiet",  # no text output
+                            "-an",  # disable audio
+                            "-f", "image2pipe",
+                            "-pix_fmt", "bgr24",
+                            "-vcodec", "rawvideo", "-"],
+                           stdin=sp.PIPE, stdout=sp.PIPE)
+    return stream_pipe, int(frame_size[0]), int(frame_size[1])
 
-# cap = cv2.VideoCapture('https://www.bogotobogo.com/VideoStreaming/VLC/Images/SimpleHttpStreaming/planet_small.mp4')
 
-# if not cap.isOpened():
-#    print("Cannot open stream")
-#    exit()
+def retrieve_next_frame_from_stream(pipe, w, h):
+    raw_image = pipe.stdout.read(w * h * 3)
+    return numpy.frombuffer(raw_image, numpy.uint8).reshape((h, w, 3))
 
-# while True:
-#     ret, frame = cap.read()
-#     # if frame is read correctly ret is True
-#     if not ret:
-#         print("Can't receive frame (stream end?). Exiting ...")
-#         break
-#     cv2.imshow('live cam', gray)
-#     if cv2.waitKey(25) == ord('q'):
-#         break
 
-# When everything done, release the capture
-# cap.release()
-# cv2.destroyAllWindows()
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='Show an m3u8 stream over http')
+    parser.add_argument(
+        'stream_link',
+        type=str,
+        help='Enter the link to an m3u8 stream over http',
+    )
+
+    stream, width, height = get_video_stream_from_url(parser.parser.parse_args().stream_link)
+
+    while True:
+        cv2.imshow("Stream", retrieve_next_frame_from_stream(stream, width, height))
+        if cv2.waitKey(30) == ord('q'):
+            break
+
+    cv2.destroyAllWindows()
