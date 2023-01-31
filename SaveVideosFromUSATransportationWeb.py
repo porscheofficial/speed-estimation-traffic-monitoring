3f7d47c17180bed482e7c2dca00ea53b1d344929
shenliu <shen.liu@porsche.digital>
2023-01-31 12:19:07 +0100

    new filw
    Former-commit-id: 2a09160712682ea7b249fadfb1368b6aea0cae9a
    Former-commit-id: 887883644bfd5ce68fec7120e95ae7bc73853f8c

---
+++ SaveVideosFromUSATransportationWeb.py
@@ -0,0 +1,30 @@
+# SaveVideosFromUSATransportationWeb
+from csv import reader
+import cv2 as cv
+import ffmpeg
+
+
+def download_video(input_url, t=180):  # save video for 3 minutes
+
+    output_path = f"{input_url.split('/')[-2]}.mp4"
+    stream = ffmpeg.input(input_url)
+    stream = ffmpeg.output(stream, filename=output_path, t=t, loglevel="quiet")
+    ffmpeg.run(stream, overwrite_output=True)
+    return  # f"File saved here in {output_path}"
+
+
+# skip the first line as it is the head line
+with open("urls1_.csv") as read_obj:
+    csv_reader = reader(read_obj)
+    header = next(csv_reader)
+
+    if header != None:
+        for row in csv_reader:
+            print(row)
+            url = row[2]
+            print(url)
+            cap = cv.VideoCapture(url)
+            if cap is None or not cap.isOpened():
+                print("unable to open the source:", url)
+            else:
+                download_video(url)
