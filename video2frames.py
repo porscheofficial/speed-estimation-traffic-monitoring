a543a64b7d60cbb284f5495a4775ae30cff8eb47
shenliu <shen.liu@porsche.digital>
2023-01-31 11:39:46 +0100

    create frames
    Former-commit-id: 13a04cbf06fe37059a752e182084635f2fcc1662
    Former-commit-id: 1df7be726dd6c2b14616cda19b40c89062616c5f

---
+++ video2frames.py
@@ -0,0 +1,37 @@
+import cv2
+import os
+import glob
+
+videoPath = '/Users/p393919/Documents/os/HamptonRoads/*.mp4'
+videos = glob.glob(videoPath)
+
+j = 0
+
+for video in sorted(videos):
+
+    fileName = os.path.basename(video)
+    fileName = os.path.splitext(fileName)[0]
+
+    parentDir = '/Users/p393919/Documents/os/frames'
+
+    newDir = os.path.join(parentDir, fileName)
+    os.mkdir(newDir)
+
+    inputpath =  os.path.abspath(video)
+    
+    outpath = os.path.abspath(newDir)
+    
+    # Opens the Video file
+    cap= cv2.VideoCapture(inputpath)
+    i=0
+    while(cap.isOpened()):
+        ret, frame = cap.read()
+        if ret == False:
+            break
+        cv2.imwrite(os.path.join(outpath, fileName + '_' + str(i).zfill(4)+'.png'),frame)
+        i+=1
+
+    cap.release()
+    cv2.destroyAllWindows()
+
+    j = j + 1 
